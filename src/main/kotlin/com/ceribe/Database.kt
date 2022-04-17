82ab09a70fdfd59e42cbc5bcee1183a3f13172b1
ceribe <ceribekagami@gmail.com>
2022-04-18 00:30:39 +0200

    Add models and "database"

--- src/main/kotlin/com/ceribe/Database.kt
+++ src/main/kotlin/com/ceribe/Database.kt
@@ -1,7 +1,11 @@
 package com.ceribe
 
 import com.ceribe.models.Pepper
+import com.ceribe.models.Pot
 
 object Database {
     val peppers = mutableListOf<Pepper>()
+    val pots = mutableListOf<Pot>()
+    var waterAmount = 0
+    var soilAmount = 0
 }
--- src/main/kotlin/com/ceribe/models/Pepper.kt
+++ src/main/kotlin/com/ceribe/models/Pepper.kt
@@ -3,4 +3,7 @@ package com.ceribe.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Pepper(val id: String, val name: String)
+data class Pepper(val id: Int, val name: String, val pot: String, val lastWatering: String)
+
+@Serializable
+data class Pot(val id: Int, val name: String, val count: Int)
