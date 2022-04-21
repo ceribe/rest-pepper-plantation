58a9054469731bcbec531ad70e0e5654e86aba5a
Ceribe <ceribekagami@gmail.com>
2022-04-21 21:18:51 +0200

    Refactor

--- src/main/kotlin/com/ceribe/route/PepperRoutes.kt
+++ src/main/kotlin/com/ceribe/route/PepperRoutes.kt
@@ -23,74 +23,80 @@ fun Route.pepperRouting() {
             val createdPepperId = Database.addDummyPepper()
             call.respond(HttpStatusCode.Created, "Pepper created with id: $createdPepperId")
         }
-        route("{id}") {
-            get {
-                val id = call.parameters["id"]!!.toInt()
-                val pepper = Database.getPepperById(id)
-                if (pepper != null) {
-                    call.respond(pepper)
-                } else {
-                    call.respond(HttpStatusCode.NotFound, "No pepper found with id: $id")
-                }
+    }
+    
+    route("peppers/{id}") {
+        get {
+            val id = call.parameters["id"]!!.toInt()
+            val pepper = Database.getPepperById(id)
+            if (pepper != null) {
+                call.respond(pepper)
+            } else {
+                call.respond(HttpStatusCode.NotFound, "No pepper found with id: $id")
             }
-            //curl -H Content-Type:application/json -X PUT --data {"name":"Reaper","pot":1,"lastWatering":"0"} http://localhost:8080/peppers/1
-            put {
-                val id = call.parameters["id"]!!.toInt()
-                if (Database.doesPepperExist(id)) {
-                    val newPepper = call.receive<Pepper>()
-                    newPepper.lastWatering = System.currentTimeMillis()
-                    if (Database.getPotCountById(newPepper.potId) == 0) {
-                        call.respond(HttpStatusCode.BadRequest, "Pot with id: ${newPepper.potId} does not exist")
-                    } else {
-                        Database.updatePepper(id, newPepper)
-                        call.respond(HttpStatusCode.OK, "Pepper updated")
-                    }
+        }
+        //curl -H Content-Type:application/json -X PUT --data {"name":"Reaper","pot":1,"lastWatering":"0"} http://localhost:8080/peppers/1
+        put {
+            val id = call.parameters["id"]!!.toInt()
+            if (Database.doesPepperExist(id)) {
+                val newPepper = call.receive<Pepper>()
+                newPepper.lastWatering = System.currentTimeMillis()
+                if (Database.getPotCountById(newPepper.potId) == 0) {
+                    call.respond(HttpStatusCode.BadRequest, "Pot with id: ${newPepper.potId} does not exist")
                 } else {
-                    call.respond(HttpStatusCode.NotFound, "No pepper found with id: $id")
+                    Database.updatePepper(id, newPepper)
+                    call.respond(HttpStatusCode.OK, "Pepper updated")
                 }
+            } else {
+                call.respond(HttpStatusCode.NotFound, "No pepper found with id: $id")
             }
-            delete {
-                val id = call.parameters["id"]!!.toInt()
-                val wasPepperDeleted = Database.deletePepperById(id)
-                if (wasPepperDeleted) {
-                    call.respond(HttpStatusCode.OK, "Pepper deleted")
-                } else {
-                    call.respond(HttpStatusCode.NotFound, "No pepper found with id: $id")
-                }
+        }
+        delete {
+            val id = call.parameters["id"]!!.toInt()
+            val wasPepperDeleted = Database.deletePepperById(id)
+            if (wasPepperDeleted) {
+                call.respond(HttpStatusCode.OK, "Pepper deleted")
+            } else {
+                call.respond(HttpStatusCode.NotFound, "No pepper found with id: $id")
             }
-            post ("waterings") {
-                val id = call.parameters["id"]!!.toInt()
-                if (Database.doesPepperExist(id)) {
-                    if (Database.waterAmount > 1) {
-                        Database.waterAmount -= 1
-                        Database.getPepperById(id)?.lastWatering = System.currentTimeMillis()
-                        call.respond(HttpStatusCode.OK, "Pepper watered")
-                    } else {
-                        call.respond(HttpStatusCode.BadRequest, "Not enough water")
-                    }
-                } else {
-                    call.respond(HttpStatusCode.NotFound, "No pepper found with id: $id")
-                }
-            }
+        }
+    }
 
-            post ("repottings") {
-                val id = call.parameters["id"]!!.toInt()
-                val pepper = Database.getPepperById(id)
-                if (pepper != null) {
-                    val potId = call.receive<String>().toInt()
-                    if (Database.getPotCountById(potId) > 0) {
-                        val wasPepperRepotted = Database.repotPepper(pepper, potId)
-                        if (wasPepperRepotted) {
-                            call.respond(HttpStatusCode.OK, "Pepper repotted")
-                        } else {
-                            call.respond(HttpStatusCode.BadRequest, "Not enough soil")
-                        }
+    route ("peppers/{id}/waterings") {
+        post {
+            val id = call.parameters["id"]!!.toInt()
+            if (Database.doesPepperExist(id)) {
+                if (Database.waterAmount > 1) {
+                    Database.waterAmount -= 1
+                    Database.getPepperById(id)?.lastWatering = System.currentTimeMillis()
+                    call.respond(HttpStatusCode.OK, "Pepper watered")
+                } else {
+                    call.respond(HttpStatusCode.BadRequest, "Not enough water")
+                }
+            } else {
+                call.respond(HttpStatusCode.NotFound, "No pepper found with id: $id")
+            }
+        }
+    }
+
+    route ("peppers/{id}/repottings") {
+        post {
+            val id = call.parameters["id"]!!.toInt()
+            val pepper = Database.getPepperById(id)
+            if (pepper != null) {
+                val potId = call.receive<String>().toInt()
+                if (Database.getPotCountById(potId) > 0) {
+                    val wasPepperRepotted = Database.repotPepper(pepper, potId)
+                    if (wasPepperRepotted) {
+                        call.respond(HttpStatusCode.OK, "Pepper repotted")
                     } else {
-                        call.respond(HttpStatusCode.BadRequest, "No pot found with id: $potId")
+                        call.respond(HttpStatusCode.BadRequest, "Not enough soil")
                     }
                 } else {
-                    call.respond(HttpStatusCode.NotFound, "No pepper found with id: $id")
+                    call.respond(HttpStatusCode.BadRequest, "No pot found with id: $potId")
                 }
+            } else {
+                call.respond(HttpStatusCode.NotFound, "No pepper found with id: $id")
             }
         }
     }
--- src/main/kotlin/com/ceribe/route/WarehouseRoutes.kt
+++ src/main/kotlin/com/ceribe/route/WarehouseRoutes.kt
@@ -10,53 +10,60 @@ import io.ktor.server.routing.*
 fun Route.warehouseRouting() {
     route("warehouse") {
         get {
-            call.respond(HttpStatusCode.OK, "Water: ${Database.waterAmount}\nSoil: ${Database.soilAmount}\nPots: ${Database.pots}")
+            call.respond(
+                HttpStatusCode.OK,
+                "Water: ${Database.waterAmount}\nSoil: ${Database.soilAmount}\nPots: ${Database.pots}"
+            )
         }
-        route("water") {
-            get {
-                call.respond(HttpStatusCode.OK, "${Database.waterAmount}")
-            }
+    }
 
-            put {
-                val body = call.receive<String>()
-                Database.waterAmount = body.toInt()
-                call.respond(HttpStatusCode.OK, "Water: ${Database.waterAmount}")
+    route("warehouse/water") {
+        get {
+            call.respond(HttpStatusCode.OK, "${Database.waterAmount}")
+        }
+
+        put {
+            val body = call.receive<String>()
+            Database.waterAmount = body.toInt()
+            call.respond(HttpStatusCode.OK, "Water: ${Database.waterAmount}")
+        }
+    }
+
+    route("warehouse/soil") {
+        get {
+            call.respond(HttpStatusCode.OK, "${Database.soilAmount}")
+        }
+        put {
+            val body = call.receive<String>()
+            Database.soilAmount = body.toInt()
+            call.respond(HttpStatusCode.OK, "Soil: ${Database.soilAmount}")
+        }
+    }
+
+    route ("warehouse/pots") {
+        get {
+            val pots = Database.pots.filter { it.count > 0 }
+            if (pots.isNotEmpty()) {
+                call.respond(HttpStatusCode.OK, pots.toString())
+            } else {
+                call.respond(HttpStatusCode.NotFound, "No unused pots found")
             }
         }
-        route("soil") {
-            get {
-                call.respond(HttpStatusCode.OK, "${Database.soilAmount}")
-            }
-            put {
-                val body = call.receive<String>()
-                Database.soilAmount = body.toInt()
-                call.respond(HttpStatusCode.OK, "Soil: ${Database.soilAmount}")
-            }
+        post {
+            val createPotId = Database.addDummyPot()
+            call.respond(HttpStatusCode.Created, "Pot created with id: $createPotId")
         }
-        route ("pots") {
-            get {
-                val pots = Database.pots.filter { it.count > 0 }
-                if (pots.isNotEmpty()) {
-                    call.respond(HttpStatusCode.OK, pots.toString())
-                } else {
-                    call.respond(HttpStatusCode.NotFound, "No unused pots found")
-                }
-            }
-            post {
-                val createPotId = Database.addDummyPot()
-                call.respond(HttpStatusCode.Created, "Pot created with id: $createPotId")
-            }
-            route("{id}") {
-                get {
+    }
 
-                }
-                put {
+    route("warehouse/pots/{id}") {
+        get {
 
-                }
-                delete {
+        }
+        put {
 
-                }
-            }
+        }
+        delete {
+
         }
     }
 }
